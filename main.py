0f81bbc7d0d7c585fa9e07c6b7f025501d9bb33d
leoperezx <leoperez.x@gmail.com>
2026-01-13 22:04:24 -0500

    actualizando main.py

--- main.py
+++ main.py
@@ -21,35 +21,42 @@ st.header('Bienvenido al panel de datos')
 st.text(f'El conjunto de datos tiene {len(df_matriculas)} registros de matrículas pero, sólo se muestran las 5 primeras filas. ')
 st.dataframe(df_matriculas.head())
 
-# tabs = st.tabs(['Datos generales','Limpieza de datos'])
-# with tabs[0]:
-#     st.subheader('Datos Generales')
-#     # Creo una lista de las universidades
-#     lista_de_universidades = df_matriculas['INSTITUCIÓN DE EDUCACIÓN SUPERIOR (IES)'].unique()
-#     # contateno la lista de universidades con la "Todas".
-#     lista_universidades_con_todas = ['Todas'] + list(lista_de_universidades)
-#     # Creo una sidebar para tomar una opción
-#     universidad_seleccionada = st.sidebar.selectbox('Selecciona la Institución:',lista_universidades_con_todas)
-#     # creo un condicional para determinar que tan grande es la selección
-#     if universidad_seleccionada == 'Todas':
-#         df_filtrado = df_matriculas
-#     else:
-#         # Comando clave de Pandas para filtrar:
-#         df_filtrado = df_matriculas[df_matriculas['INSTITUCIÓN DE EDUCACIÓN SUPERIOR (IES)'] == universidad_seleccionada]
-#     # Título de la entrega de la información    
-#     st.subheader(f'Datos Filtrados para: {universidad_seleccionada}')
-#     # conteo de los registro de matrícula de acuerdo a la selección
-#     st.write(f'Número de registros: **{len(df_filtrado)}**')
-#     # muestra la información flitrada
-#     st.dataframe(df_filtrado,hide_index=True)
-
-#     matriculas_por_ies = fn.matriculas_por_ies(df_matriculas)
-#     # print(matriculas_por_ies)
+tabs = st.tabs(['Info por departamento','Limpieza de datos'])
+with tabs[0]:
+    st.subheader('Info por departamento')
+    # Creo una lista de las universidades
+    lista_de_departamentos = df_matriculas['DEPARTAMENTO DE OFERTA DEL PROGRAMA'].unique()
+    # contateno la lista de universidades con la "Todas".
+    lista_departamentos = list(lista_de_departamentos)
+    # Creo una sidebar para tomar una opción
     
-#     st.dataframe(matriculas_por_ies)
-#     # print(matriculas_por_ies[0].dtypes)
-#     # sumatoria_por_seleccion_de_ies = matriculas_por_ies[1].sum()
-#     # st.text(f'total de matriculados es: {sumatoria_por_seleccion_de_ies}')
+    departamento_seleccionado = st.selectbox('Selecciona el departamento:',lista_departamentos)
+    # creo un condicional para determinar que tan grande es la selección
+    # df_filtrado = df_matriculas[df_matriculas['INSTITUCIÓN DE EDUCACIÓN SUPERIOR (IES)'] == departamento_seleccionado]
+    # Título de la entrega de la información    
+    st.subheader(f'Datos Filtrados para: {departamento_seleccionado}')
+    
+    df_departamento=fn.df_filtro_departamento(df_matriculas,departamento_seleccionado)
+    # conteo de los registro de matrícula de acuerdo a la selección
+    st.write(f'Número de registros: **{len(df_departamento)}**')
+    # muestra la información flitrada
+    # st.dataframe(df_filtrado,hide_index=True)
 
-# with tabs[1]:
-#      st.subheader('Limpieza de datos')
+    matriculas_por_departamento = fn.matriculas_por_departamento(df_departamento)
+    # print(matriculas_por_ies)
+    
+    st.dataframe(matriculas_por_departamento)
+    # print(matriculas_por_ies[0].dtypes)
+    sumatoria_por_IES = fn.top_10_IES_sumatoria(df_departamento)
+    print(sumatoria_por_IES.sort_values(ascending=False).head(20))
+    
+    # tareas
+    # - imprimir lista de IES en streamlit
+    # - hacer graficas (torta o barras con porcentajes )
+    # - separar info de posgrados 
+    # - imprimir lista por áreas
+    # - separar info por programas
+    # - hacer graficas de los programas (torta o barras con porcentajes)
+
+with tabs[1]:
+     st.subheader('Limpieza de datos')
--- src/funciones.py
+++ src/funciones.py
@@ -17,6 +17,12 @@ def cargar_datos(archivo):
     # Asegúrate de usar la ruta correcta a tu archivo
     return pd.read_csv(archivo)
 
-def matriculas_por_ies(archivo):
-    return archivo.groupby('INSTITUCIÓN DE EDUCACIÓN SUPERIOR (IES)')['MATRICULADOS'].sum().reset_index()    
+def matriculas_por_departamento(archivo):
+    return archivo.groupby('DEPARTAMENTO DE OFERTA DEL PROGRAMA')['MATRICULADOS'].sum().reset_index()    
+
+def df_filtro_departamento(df,departamento):
+    return df[df['DEPARTAMENTO DE OFERTA DEL PROGRAMA'].isin([departamento])] 
+
+def top_10_IES_sumatoria(df):
+    return df.groupby('INSTITUCIÓN DE EDUCACIÓN SUPERIOR (IES)')['MATRICULADOS'].sum()   
     
